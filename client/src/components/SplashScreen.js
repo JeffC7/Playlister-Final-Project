8c83c931ae763948ddc1aebf1d22325ac7f96446
JeffC7 <jeffreychin20@gmail.com>
2022-12-04 06:20:41 -0500

    "Created Splash Screen. Fixed some CSS with it"

--- client/src/components/SplashScreen.js
+++ client/src/components/SplashScreen.js
@@ -2,7 +2,17 @@ export default function SplashScreen() {
     
     return (
         <div id="splash-screen">
-            Playlister            
+            <div class="splash-screen-text">
+                Welcome!
+            </div>
+            
+            <div class="splash-screen-text2"> 
+                Create your own unique playlist and share with the world!
+            </div>
+
+            <div class="splash-screen-text3">
+                Designed by: Jeffrey Chin
+            </div>
         </div>
     )
 }
